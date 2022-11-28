d60d3c99dfff8570fa76ba287236183b89c1833a
Oshinko <osnk@renjaku.jp>
2022-11-28 23:50:01 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,15 @@ mod v2 {
     }
 
     impl Expression {
-        fn parse_field(field: &str) -> Field {
+        fn parse_field(field: &str) -> UnfixedField {
             let re_all = Regex::new(r"^\*(?:/(\d+))?$").unwrap();
-            let re_range = Regex::new(r"^(\d+)-(\d+)(?:/(\d+))?$").unwrap();
-            let re_single = Regex::new(r"^(\d+)(?:/(\d+))?$").unwrap();
+            let re_range = Regex::new(r"^([^-]+)-([^/]+)(?:/(\d+))?$").unwrap();
+            let re_single = Regex::new(r"^([^/]+)(?:/(\d+))?$").unwrap();
 
             let mut patterns = Vec::new();
 
             for pattern_str in field.split(',') {
-                let pattern: TimePattern;
+                let pattern: UnfixedTimePattern;
 
                 if let Some(caps) = re_all.captures(pattern_str) {
                     let step: Option<u8>;
@@ -37,10 +37,10 @@ mod v2 {
                         step = None;
                     }
 
-                    pattern = TimePattern::All(step);
+                    pattern = UnfixedTimePattern::All(step);
                 } else if let Some(caps) = re_range.captures(pattern_str) {
-                    let a = caps.get(1).unwrap().as_str().parse::<u8>().unwrap();
-                    let b = caps.get(2).unwrap().as_str().parse::<u8>().unwrap();
+                    let a = caps.get(1).unwrap().as_str();
+                    let b = caps.get(2).unwrap().as_str();
                     let step: Option<u8>;
 
                     if let Some(c) = caps.get(3) {
@@ -49,9 +49,9 @@ mod v2 {
                         step = None;
                     }
 
-                    pattern = TimePattern::Range(a, b, step);
+                    pattern = UnfixedTimePattern::Range(a, b, step);
                 } else if let Some(caps) = re_single.captures(pattern_str) {
-                    let a = caps.get(1).unwrap().as_str().parse::<u8>().unwrap();
+                    let a = caps.get(1).unwrap().as_str();
                     let step: Option<u8>;
 
                     if let Some(c) = caps.get(2) {
@@ -60,7 +60,7 @@ mod v2 {
                         step = None;
                     }
 
-                    pattern = TimePattern::Single(a, step);
+                    pattern = UnfixedTimePattern::Single(a, step);
                 } else {
                     panic!("invalid field: {}", field);
                 }
@@ -68,22 +68,40 @@ mod v2 {
                 patterns.push(pattern);
             }
 
-            Field { patterns }
+            UnfixedField { patterns }
         }
 
-        fn fix_days_of_week(mut field: Field) -> Field {
-            let mut pats = Vec::new();
+        fn str2weeknum(s: &str) -> u8 {
+            if s.chars().all(|x| x.is_numeric()) {
+                let n = s.parse::<u8>().unwrap();
+                if n == 0 {
+                    7
+                } else {
+                    n
+                }
+            } else {
+                s.parse::<Weekday>()
+                    .unwrap()
+                    .number_from_monday()
+                    .try_into()
+                    .unwrap()
+            }
+        }
 
-            for pat in field.patterns.iter() {
-                let pat2: TimePattern;
+        fn fix_days_of_week(field: UnfixedField) -> Field {
+            let mut patterns = Vec::new();
 
-                match pat {
-                    TimePattern::All(step) => {
-                        pat2 = TimePattern::All(*step);
+            for unfixed in field.patterns.iter() {
+                let fixed: TimePattern;
+
+                match unfixed {
+                    UnfixedTimePattern::All(step) => {
+                        fixed = TimePattern::All(*step);
                     }
-                    TimePattern::Range(start, end, step) => {
-                        let mut start2 = if *start == 0 { 7 } else { *start };
-                        let mut end2 = if *end == 0 { 7 } else { *end };
+
+                    UnfixedTimePattern::Range(start, end, step) => {
+                        let mut start2 = Self::str2weeknum(start);
+                        let mut end2 = Self::str2weeknum(end);
 
                         if end2 < start2 {
                             let tmp = start2;
@@ -91,29 +109,59 @@ mod v2 {
                             end2 = tmp;
                         }
 
-                        pat2 = TimePattern::Range(start2, end2, *step);
+                        fixed = TimePattern::Range(start2, end2, *step);
                     }
-                    TimePattern::Single(value, step) => {
-                        pat2 = TimePattern::Single(if *value == 0 { 7 } else { *value }, *step);
+
+                    UnfixedTimePattern::Single(value, step) => {
+                        fixed = TimePattern::Single(Self::str2weeknum(value), *step);
                     }
                 }
 
-                pats.push(pat2);
+                patterns.push(fixed);
             }
 
-            field.patterns = pats;
-            field
+            Field { patterns }
+        }
+
+        fn fix(field: UnfixedField) -> Field {
+            let mut patterns = Vec::new();
+
+            for unfixed in field.patterns.iter() {
+                let fixed: TimePattern;
+
+                match unfixed {
+                    UnfixedTimePattern::All(step) => {
+                        fixed = TimePattern::All(*step);
+                    }
+
+                    UnfixedTimePattern::Range(start, end, step) => {
+                        fixed = TimePattern::Range(
+                            start.parse::<u8>().unwrap(),
+                            end.parse::<u8>().unwrap(),
+                            *step,
+                        );
+                    }
+
+                    UnfixedTimePattern::Single(value, step) => {
+                        fixed = TimePattern::Single(value.parse::<u8>().unwrap(), *step);
+                    }
+                }
+
+                patterns.push(fixed);
+            }
+
+            Field { patterns }
         }
 
         fn parse(text: &str) -> (Self, SplitWhitespace) {
             let mut iter = text.split_whitespace();
-            let minutes = Self::parse_field(iter.next().unwrap());
-            let hours = Self::parse_field(iter.next().unwrap());
-            let days = Self::parse_field(iter.next().unwrap());
-            let months = Self::parse_field(iter.next().unwrap());
+            let minutes = Self::fix(Self::parse_field(iter.next().unwrap()));
+            let hours = Self::fix(Self::parse_field(iter.next().unwrap()));
+            let days = Self::fix(Self::parse_field(iter.next().unwrap()));
+            let months = Self::fix(Self::parse_field(iter.next().unwrap()));
 
-            let mut days_of_week = Self::parse_field(iter.next().unwrap());
-            days_of_week = Self::fix_days_of_week(days_of_week);
+            let unfixed_days_of_week = Self::parse_field(iter.next().unwrap());
+            let days_of_week = Self::fix_days_of_week(unfixed_days_of_week);
 
             (
                 Self {
@@ -196,21 +244,7 @@ mod v2 {
     fn test() {
         assert!(TimePattern::All(None) == TimePattern::All(None));
 
-        // minutes: 0-59
-        // hours: 0-23
-        // days: 1-31
-        // months: 1-12
-        // days_of_week: 0-7
-
-        // minutes: 0-10
-        // now.minute: 1
-        // return latest: 1
-
-        // minutes: 0-10
-        // now.minute: 11
-        // return latest: 10
-
-        // let dt = Local::now();
+        // 2022-11-27 (Sun) 19:01:00.000
         let dt = NaiveDate::from_ymd_opt(2022, 11, 27)
             .unwrap()
             .and_hms_milli_opt(19, 1, 00, 00)
@@ -220,17 +254,26 @@ mod v2 {
 
         println!("#### DateTime: {:?}", dt);
 
-        let expression = "1 19 27 11 0";
-        let (expr, mut iter) = Expression::parse(expression);
+        let mut expression = "1 19 27 11 7";
+        let (mut expr, _) = Expression::parse(expression);
 
         println!("#### Expression: {:?} to {:?}", expression, expr);
 
-        // println!("#### weekday? {:?}", dt.weekday().number_from_monday());
-        // println!("#### weekday? {:?}", dt.weekday().to_string().parse::<Weekday>().unwrap());
-        println!("{:?}", expr.test(dt));
+        assert!(expr.test(dt));
 
-        let s = "aaaa";
-        println!("{:?}", UnfixedTimePattern::Single(s, None));
+        expression = "1 19 27 11 0"; // days_of_week 0 == 7
+        (expr, _) = Expression::parse(expression);
+
+        println!("#### Expression: {:?} to {:?}", expression, expr);
+
+        assert!(expr.test(dt));
+
+        expression = "0 19 27 11 7";
+        (expr, _) = Expression::parse(expression);
+
+        println!("#### Expression: {:?} to {:?}", expression, expr);
+
+        assert!(!expr.test(dt));
 
         panic!("##### its dummy error"); // for debug
     }
